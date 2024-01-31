352f6cf87e58263921b07169e2ba2734baf19574
Anson Joseph <ansonpjp@gmail.com>
2024-02-01 08:41:22 +1300

    1-mysql connection established

--- app/index.php
+++ app/index.php
@@ -1,3 +1,14 @@
-<?php 
-    echo '<h1>Hello Wghggggggorld!</h1>'; 
-?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Employee Management</title>
+</head>
+<body>
+    <h1>Employee Management</h1>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        Select CSV file: <input type="file" name="file" required>
+        <input type="submit" value="Upload">
+    </form>
+</body>
+</html>
